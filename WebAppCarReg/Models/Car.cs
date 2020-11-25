47d8e886ba8f98a1cbe4f7e24b403a8aea359ab9
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-11-25 08:54:22 +0100

    small adjustments made

--- WebAppCarReg/Models/Car.cs
+++ WebAppCarReg/Models/Car.cs
@@ -11,5 +11,19 @@ namespace WebAppCarReg.Models
         public string Brand { get; set; }
         public string ModelName { get; set; }
         public int Year { get; set; }
+
+        public Car() {}
+
+        public Car(string brand, string modelName, int year)
+        {
+            Brand = brand;
+            ModelName = modelName;
+            Year = year;
+        }
+
+        public Car(int id, string brand, string modelName, int year) : this(brand,modelName,year)
+        {
+            Id = id;
+        }
     }
 }
