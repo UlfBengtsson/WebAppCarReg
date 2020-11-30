3752425b98a01260180ae32057272b19ebc995b2
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-11-30 11:56:28 +0100

    added asp-append-version to avoid web browser charing files that was changed

---
+++ WebAppCarReg/wwwroot/js/CarsUtil.js
@@ -0,0 +1,16 @@
+﻿
+
+function findById(event, urlHelper) {
+    console.log(urlHelper);
+    event.preventDefault();
+    const anchorElement = event.target;
+    const inputIdValue = $("#carIdInput").val();
+
+    $.get(anchorElement.attributes.href.value + "/" + inputIdValue, function (result) {
+
+        $('#' + anchorElement.attributes["data-target"].value).html(result);
+
+    }).fail(function () {
+        alert("Car with this Id: " + inputIdValue + " was not found");
+    });
+};
