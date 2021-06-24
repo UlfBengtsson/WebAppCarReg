f1862e95b4b04ebf8d0a4928da2dcfdc8a12ada2
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2021-06-24 09:43:39 +0200

    made generic Repo interface

---
+++ WebAppCarReg/Models/Data/CarRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCarReg.Models.Data
+{
+    public class CarRepo : IGenericRepo<Car, CreateCarViewModel>
+    {
+        public Car Create(CreateCarViewModel create)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Car model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Update(Car model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebAppCarReg/Models/Data/IGenericRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCarReg.Models.Data
+{
+    public interface IGenericRepo<ModelType, CreateType>
+    {
+        //CRUD
+        ModelType Create(CreateType create);
+
+        ModelType Read(int id);
+        List<ModelType> Read();
+
+        ModelType Update(ModelType model);
+
+        bool Delete(ModelType model);
+    }
+}
